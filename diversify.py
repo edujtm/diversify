0c939a53b357e717aa61596723bfbe761ee2e7d5
edujtm <eduzemacedo@hotmail.com>
2018-11-19 20:32:46 -0300

    Added description to diversify.py, fixed genetic.py when using only one user

--- diversify.py
+++ diversify.py
@@ -4,7 +4,7 @@ import interfacespfy as isp
 import genetic as gen
 import pandas as pd
 import pprint
-import splearn as sp
+import argparse
 
 twousers = False
 
@@ -17,30 +17,59 @@ def get_songs(spfy, userid):
         return pd.DataFrame(result)
 
 
-# TODO use argparse to parse CLI
-plistname = None
+description_hint = """
+        DIVERSIFY PLAYLIST GENERATOR
+        ----------------------------
+        
+        This program will create a new playlist in your account based on your 
+        and your friend's Spotify public playlists, trying to please both of 
+        your musical tastes.
+        
+        The playlist will be saved on the account of the first user.
 
-if len(sys.argv) > 1:
-    username1 = sys.argv[1]
-    username2 = sys.argv[2]
-    plistname = ' '.join(sys.argv[3:])
-else:
-    print("Usage: python3 {0} <username1> <username2> [playlist name]".format(sys.argv[0]))
-    sys.exit()
+        The app will redirect you to a page in your browser,
+        where you will be asked to login into your spotify account.
+
+        After logging in into your account, you should be redirected to a page
+        with the URL pattern as follows:
+
+        http://localhost/?code={code-pattern}
+
+        where you should copy the code-pattern and paste into your terminal. 
+        These steps are only necessary once and are a current limitation of the
+        spotify WEB API.
+
+        If you prefer, you can also log in through the spotify website into your 
+        browser and then run this program, which will not ask for your credentials
+        as you'll be already logged in.
+
+        Spotify website: https://www.spotify.com/
+    """
+
+parser = argparse.ArgumentParser(description=description_hint, formatter_class=argparse.RawTextHelpFormatter)
+
+parser.add_argument('user1', help='Your Spotify URI')
+parser.add_argument('-u2', '--user2', dest='user2', help="Your friend's Spotify URI")
+parser.add_argument('-p', '--playlist_name', nargs='+', default=['Divesify playlist'],
+                    dest='playlist_name',
+                    help='The name for the playlist that will be created')
+args = parser.parse_args()
+
+plistname = ' '.join(args.playlist_name)
+username1 = args.user1
+username2 = args.user2
 
 spfy = isp.login_user(username1)
 
-sp.load_mlearn(spfy, users=[username1, username2])
-"""
 user1 = get_songs(spfy, username1)
-user2 = get_songs(spfy, username2)
 
-result = gen.start(spfy, user1, user2=user2)
-
-if plistname is None:
-    plistname = 'Diversify Playlist'
+if username2 is not None:
+    user2 = get_songs(spfy, username2)
+    result = gen.start(spfy, user1, user2=user2)
+else:
+    print("empty user2")
+    result = gen.start(spfy, user1)
 
 pprint.pprint(result)
 resultids = result.index.tolist()
-isp.tracks_to_playlist(spfy, 'belzedu', trackids=resultids, name=plistname)
-"""
+isp.tracks_to_playlist(spfy, username1, trackids=resultids, name=plistname)
